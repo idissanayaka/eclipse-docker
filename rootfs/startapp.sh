3850205b8e5cc1a5a43013b8fefc6ce16a2f70fe
Iresh Dissanayaka <iresh@n-able.biz>
2024-10-07 08:07:26 +0000

    small fixes + additions to rootfs

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,5 +3,6 @@ export HOME=/config
 
 # "fix" homedir for app user as Eclipse ignores $HOME (Java user.home != $HOME)
 #sed -i "/^${APP_USER}:/ { s#/dev/null#${HOME}# }" /etc/passwd
+take-ownership /config
 
 exec /opt/eclipse/eclipse -vmargs "-Duser.home=$HOME"
